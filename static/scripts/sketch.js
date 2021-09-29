71420760145269d756841c2f5e599aa6ae913121
st167058 <st167058@stud.uni-stuttgart.de>
2021-09-29 23:55:30 +0200

    optimized langtons ant script by rendering fixed number of steps at once instead of every single loop

--- static/scripts/sketch.js
+++ static/scripts/sketch.js
@@ -1,5 +1,5 @@
-let w = 40;
-let h = 40;
+let w = 400;
+let h = 400;
 
 let x = w/2;
 let y = h/2;
@@ -7,14 +7,11 @@ let direction = 0;
 
 let grid = [];
 
+let speed = 500;
 
 function setup() {
-    let reader = new FileReader();
-    
-
-    createCanvas(400, 400);
+    createCanvas(w, h);
     background(70);
-    frameRate(10000); 
     for (let i = 0; i < h; i++) {
         grid[i] = [];
         for (let j = 0; j < w; j++) {
@@ -25,26 +22,28 @@ function setup() {
 }
 
 function draw() {
-    if (grid[x][y]%2 == 0) {
-        direction = (direction + 1)%4;  
-        color = 255;
-    } else {
-        direction = ((direction - 1)%4 + 4)%4;
-        color = 0;
-    }
-    
-    fill(color);
-    grid[x][y] += 1;
-    rect(x*10, y*10, 10, 10);
-    
+    for (let loop = 0; loop < speed; loop++){
+        if (grid[x][y]%2 == 0) {
+            direction = (direction + 1)%4;  
+            color = 255;
+        } else {
+            direction = ((direction - 1)%4 + 4)%4;
+            color = 0;
+        }
+        
+        fill(color);
+        grid[x][y] += 1;
+        rect(x, y, 1, 1);
+        
 
-    if (direction == 0) {
-        x = (x + 1)%w;
-    } else if (direction == 1) {
-        y = ((y - 1)%h + h)%h;
-    } else if (direction == 2) {
-        x = ((x - 1)%w + w)%w;
-    } else {
-        y = (y + 1)%h;
+        if (direction == 0) {
+            x = (x + 1)%w;
+        } else if (direction == 1) {
+            y = ((y - 1)%h + h)%h;
+        } else if (direction == 2) {
+            x = ((x - 1)%w + w)%w;
+        } else {
+            y = (y + 1)%h;
+        }
     }
 }
