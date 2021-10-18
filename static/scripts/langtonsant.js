f4b696752f57589a1b7deccdf236a91b2e5eb7d1
st167058 <st167058@stud.uni-stuttgart.de>
2021-10-18 17:51:18 +0200

    omplement canvas controls for langtonsant

--- static/scripts/langtonsant.js
+++ static/scripts/langtonsant.js
@@ -1,30 +1,20 @@
-/* HTML functionality */
+let running = false;
+let grid = [];
+let w = 400;
+let h = 400;
+let x = w/2;
+let y = h/2;
+let direction = 0;
 
-
-let resetGrid = function (grid) {
-    for (let row = 0; row < grid.length; row++) {
-        for (let column = 0; column < row.length; column++) {
-            grid[row][column] = 0;
-        }
-    }
-    return grid;
+let start = function () {
+    running = true;
 }
 
-
-
-
-
-/* Canvas Script */
+let stop = function() {
+    running = false;
+}
 
 let s = function ( p ) {
-    let w = 400;
-    let h = 400;
-
-    let x = w/2;
-    let y = h/2;
-    let direction = 0;
-
-    let grid = [];
 
     let speed = 500;
 
@@ -41,31 +31,51 @@ let s = function ( p ) {
     }
 
     p.draw = function() {
-        for (let loop = 0; loop < speed; loop++){
-            if (grid[x][y]%2 == 0) {
-                direction = (direction + 1)%4;  
-                color = 255;
-            } else {
-                direction = ((direction - 1)%4 + 4)%4;
-                color = 0;
-            }
-            
-            p.fill(color);
-            grid[x][y] += 1;
-            p.rect(x, y, 1, 1);
-            
+        if (running) {
+            for (let loop = 0; loop < speed; loop++){
+                if (grid[x][y]%2 == 0) {
+                    direction = (direction + 1)%4;  
+                    color = 255;
+                } else {
+                    direction = ((direction - 1)%4 + 4)%4;
+                    color = 0;
+                }
+                
+                p.fill(color);
+                grid[x][y] += 1;
+                p.rect(x, y, 1, 1);
+                
 
-            if (direction == 0) {
-                x = (x + 1)%w;
-            } else if (direction == 1) {
-                y = ((y - 1)%h + h)%h;
-            } else if (direction == 2) {
-                x = ((x - 1)%w + w)%w;
-            } else {
-                y = (y + 1)%h;
+                if (direction == 0) {
+                    x = (x + 1)%w;
+                } else if (direction == 1) {
+                    y = ((y - 1)%h + h)%h;
+                } else if (direction == 2) {
+                    x = ((x - 1)%w + w)%w;
+                } else {
+                    y = (y + 1)%h;
+                }
             }
         }
     }
 }
 
 let myp5 = new p5(s, "canvas");
+
+
+let reset = function () {
+    x = w/2;
+    y = h/2;
+    direction = 0;
+    running = false;
+    for (let i = 0; i < h; i++) {
+        grid[i] = [];
+        for (let j = 0; j < w; j++) {
+            grid[i][j] = 0;
+        }
+    }
+    const canvas = document.getElementById("canvas").childNodes[1];
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#464646";
+    ctx.fillRect(0, 0, w, h);
+}
