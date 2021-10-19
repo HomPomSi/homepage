3f63c1f69a7a6c6c119bf6620370e1f1c3f8a0ff
st167058 <st167058@stud.uni-stuttgart.de>
2021-10-19 13:26:53 +0200

    better resizing of langtonsant page for mobile resolutions, tweaking some values inside the ant script

--- static/scripts/langtonsant.js
+++ static/scripts/langtonsant.js
@@ -5,6 +5,7 @@ let h = 400;
 let x = w/2;
 let y = h/2;
 let direction = 0;
+let speed = 1024;
 
 let start = function () {
     running = true;
@@ -14,9 +15,43 @@ let stop = function() {
     running = false;
 }
 
-let s = function ( p ) {
 
-    let speed = 500;
+
+let s = function ( p ) {
+    
+    let resize = function () {
+        if (document.documentElement.clientWidth <= w + 300) {
+            document.getElementById("langtonsant-full-wrapper").style.flexDirection = "column";
+        } else {
+            document.getElementById("langtonsant-full-wrapper").style.flexDirection = "";
+        }
+
+    }
+
+    let move = function() {
+        if (grid[x][y]%2 == 0) {
+            direction = (direction + 1)%4;  
+            color = 255;
+        } else {
+            direction = ((direction - 1)%4 + 4)%4;
+            color = 0;
+        }
+        
+        p.fill(color);
+        grid[x][y] += 1;
+        p.rect(x, y, 1, 1);
+        
+
+        if (direction == 0) {
+            x = (x + 1)%w;
+        } else if (direction == 1) {
+            y = ((y - 1)%h + h)%h;
+        } else if (direction == 2) {
+            x = ((x - 1)%w + w)%w;
+        } else {
+            y = (y + 1)%h;
+        }
+    }
 
     p.setup = function() {
         p.createCanvas(w, h);
@@ -31,30 +66,10 @@ let s = function ( p ) {
     }
 
     p.draw = function() {
+        resize();
         if (running) {
             for (let loop = 0; loop < speed; loop++){
-                if (grid[x][y]%2 == 0) {
-                    direction = (direction + 1)%4;  
-                    color = 255;
-                } else {
-                    direction = ((direction - 1)%4 + 4)%4;
-                    color = 0;
-                }
-                
-                p.fill(color);
-                grid[x][y] += 1;
-                p.rect(x, y, 1, 1);
-                
-
-                if (direction == 0) {
-                    x = (x + 1)%w;
-                } else if (direction == 1) {
-                    y = ((y - 1)%h + h)%h;
-                } else if (direction == 2) {
-                    x = ((x - 1)%w + w)%w;
-                } else {
-                    y = (y + 1)%h;
-                }
+                move();
             }
         }
     }
